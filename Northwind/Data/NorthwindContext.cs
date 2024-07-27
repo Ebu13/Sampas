32ec5ba867886f7b890faca3bc746038bc0ed2f8
ebu13 <ebubekir13042003@gmail.com>
2024-07-27 21:04:07 +0300

    last

--- Northwind/Data/NorthwindContext.cs
+++ Northwind/Data/NorthwindContext.cs
@@ -36,6 +36,8 @@ public partial class NorthwindContext : DbContext
 
     public virtual DbSet<Supplier> Suppliers { get; set; }
 
+    public virtual DbSet<User> Users { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=EBUBEKIR13;Database=Northwind;Trusted_Connection=True;TrustServerCertificate=True;");
@@ -242,6 +244,17 @@ public partial class NorthwindContext : DbContext
             entity.Property(e => e.SupplierName)
                 .HasMaxLength(50)
                 .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.UserId).HasName("PK__Users__1788CC4CF8BF0FC1");
+
+            entity.HasIndex(e => e.Username, "UQ__Users__536C85E41A412BF7").IsUnique();
+
+            entity.Property(e => e.Password).HasMaxLength(255);
+            entity.Property(e => e.Role).HasMaxLength(50);
+            entity.Property(e => e.Username).HasMaxLength(50);
         });
 
         OnModelCreatingPartial(modelBuilder);
